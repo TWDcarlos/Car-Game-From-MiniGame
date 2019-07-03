2857d9e699d3eee0677e03364ab2b60658ff9d5b
Carlos <carlos.henrique2324@gmail.com>
2019-07-03 15:41:55 -0300

    Update sketch.js

--- p5js Version/Car Game Neural Evolution/sketch.js
+++ p5js Version/Car Game Neural Evolution/sketch.js
@@ -1,38 +1,13 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Neuro-Evolution Flappy Bird
-
-// Part 1: https://youtu.be/c6y21FkaUqw
-// Part 2: https://youtu.be/tRA6tqgJBc
-// Part 3: https://youtu.be/3lvj9jvERvs
-// Part 4: https://youtu.be/HrvNpbnjEG8
-// Part 5: https://youtu.be/U9wiMM3BqLU
-
 const TOTAL = 50;
-//let birds = [];
-//let savedBirds = [];
-//let pipes = [];
 let counter = 0;
-let slider;
 let bF = 0;
 
 let game;
 let scl;
 
-function keyPressed() {
-  if (key === 'S') {
-    let bird = birds[0];
-    saveJSON(bird.brain, 'bird.json');
-  }
-}
-
 function setup() {
   createCanvas(270, 601);
-  //frameRate(20);
   scl = floor(width / 9);
-  slider = createSlider(1, 10, 1);
   game = new Game();
 }
 
@@ -59,70 +34,7 @@ function draw() {
 
 
   if (game.cars.length == 0) {
-    //counter = 0;
     game.nextGeneration();
     game.enemy = [];
   }
-
-
-  /*
-  for (let n = 0; n < slider.value(); n++) {
-    if (counter % 75 == 0) {
-      pipes.push(new Pipe());
-    }
-    counter++;
-
-    for (let i = pipes.length - 1; i >= 0; i--) {
-      pipes[i].update();
-
-      for (let j = birds.length - 1; j >= 0; j--) {
-        if (pipes[i].hits(birds[j])) {
-          savedBirds.push(birds.splice(j, 1)[0]);
-        }
-      }
-
-      if (pipes[i].offscreen()) {
-        pipes.splice(i, 1);
-      }
-    }
-
-    for (let i = birds.length - 1; i >= 0; i--) {
-      if (birds[i].offScreen()) {
-        savedBirds.push(birds.splice(i, 1)[0]);
-      }
-    }
-
-    for (let bird of birds) {
-      bird.think(pipes);
-      bird.update();
-    }
-
-    if (birds.length === 0) {
-      counter = 0;
-      nextGeneration();
-      pipes = [];
-    }
-
-    */
-  }
-
-  // All the drawing stuff
-  
-
-  /*
-  for (let bird of birds) {
-    bird.show();
-  }
-
-  for (let pipe of pipes) {
-    pipe.show();
-  }
-  */
-
-
-// function keyPressed() {
-//   if (key == ' ') {
-//     bird.up();
-//     //console.log("SPACE");
-//   }
-// }
+}
